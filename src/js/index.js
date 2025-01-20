b5ea18bf3bb76060c6c5250fb7fd29a275ceba4f
joseAngel33 <163841769+joseAngel33@users.noreply.github.com>
2025-01-20 11:53:36 +0100

    +update

--- src/js/index.js
+++ src/js/index.js
@@ -5,7 +5,16 @@ const menuIconElement = document.getElementById('menu-icon');
 const menuElement = document.getElementById('menu');
 
 const toggleMenu = () => {
+  //Preguntar si tiene la clase
   if (menuElement.classList.contains('menu-show')) {
+    //Si la tiene, se la quitamos y ponemos icono de hamburguesa
     menuElement.classList.remove(menu - show);
+    menuIconElement.src = './assets/images/icon-hamburger.svg';
+  } else {
+    //Si no la tiene, se la ponemos y ponemos ucono de cerrar
+    menuElement.classList.add('menu-show');
+    menuIconElement.src = './assets/images/icon-close.svg';
   }
 };
+
+menuIconElement.addEventListener('click', toggleMenu);
