cfda93e2a704e8f6885e72c635f1b62f126c5648
joseAngel33 <163841769+joseAngel33@users.noreply.github.com>
2025-01-20 09:52:41 +0100

    +update

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,11 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
+
+const menuIconElement = document.getElementById('menu-icon');
+const menuElement = document.getElementById('menu');
+
+const toggleMenu = () => {
+  if (menuElement.classList.contains('menu-show')) {
+    menuElement.classList.remove(menu - show);
+  }
+};
